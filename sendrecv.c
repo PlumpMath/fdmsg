f9f27d332197e76cf2397616a153d272a309d6ed
Ian Denhardt <ian@zenhack.net>
2016-07-22 19:07:34 -0400

    sendrecv: invert control.
    Instead of passing a callback, we just have a prep_msg helper and do the
    send/recv afterwards in the main function.
    
    This gets rid of the ugly cast, and is a bit more idiomatic C.

--- sendrecv.c
+++ sendrecv.c
@@ -1,14 +1,11 @@
 #include <string.h>
 #include "fdmsg.h"
 
-static ssize_t do_msg(
-	ssize_t (*fn)(int, struct msghdr *, int),
-	int socket,
+static void prep_msg(
+	struct msghdr *msg, struct iovec *iov,
 	void *data, size_t data_len,
 	void *fdbuf, size_t fds_count) {
 
-	struct msghdr msg;
-	struct iovec iov;
 
 	struct cmsghdr *cmsg = fdbuf;
 
@@ -16,26 +13,28 @@ static ssize_t do_msg(
 	memset(fdbuf, 0, CMSG_SPACE(0));
 
 	/* Set up the file descriptors to be tansferred: */
-	msg.msg_control = cmsg;
-	msg.msg_controllen = FDMSG_BUFSZ(fds_count);
+	msg->msg_control = cmsg;
+	msg->msg_controllen = FDMSG_BUFSZ(fds_count);
 	cmsg->cmsg_level = SOL_SOCKET;
 	cmsg->cmsg_type = SCM_RIGHTS;
 	cmsg->cmsg_len = CMSG_LEN(sizeof(int) * fds_count);
 
 	/* Set up the data to be transfered: */
-	msg.msg_iovlen = 1;
-	msg.msg_iov = &iov;
-	iov.iov_base = data;
-	iov.iov_len = data_len;
-
-	return fn(socket, &msg, 0);
+	msg->msg_iovlen = 1;
+	msg->msg_iov = iov;
+	iov->iov_base = data;
+	iov->iov_len = data_len;
 }
 
 ssize_t fdmsg_recv(int socket,
 	void *data, size_t data_len,
 	void *fdbuf, size_t fds_count) {
 
-	return do_msg(recvmsg, socket, data, data_len, fdbuf, fds_count);
+	struct msghdr msg;
+	struct iovec iov;
+
+	prep_msg(&msg, &iov, data, data_len, fdbuf, fds_count);
+	return recvmsg(socket, &msg, 0);
 }
 
 
@@ -43,8 +42,9 @@ ssize_t fdmsg_send(int socket,
 	void *data, size_t data_len,
 	void *fdbuf, size_t fds_count) {
 
-	/* we cast sendmsg here to get rid of the const qualifier, which
-	 * recvmsg does not have. this lets us do this in a generic way. */
-	return do_msg((ssize_t (*)(int, struct msghdr *, int))sendmsg,
-			socket, data, data_len, fdbuf, fds_count);
+	struct msghdr msg;
+	struct iovec iov;
+
+	prep_msg(&msg, &iov, data, data_len, fdbuf, fds_count);
+	return sendmsg(socket, &msg, 0);
 }
